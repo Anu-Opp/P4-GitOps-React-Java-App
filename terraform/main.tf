0b946d17284865c13941e915e8ea563274c8aa14
anu.opp <anu.op@outlook.com>
2025-06-13 16:52:45 +0100

    file update

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,6 +42,11 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   
+  # Enable public access to cluster endpoint
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+  
   # Enable cluster creator admin permissions
   enable_cluster_creator_admin_permissions = true
   
